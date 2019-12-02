ab1b03990084d976be4f8932b738a966f14fad0e
haya14busa <haya14busa@gmail.com>
2019-12-02 10:40:39 +0000

    Support github-check reporter

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
     | jq -r '.[] | "\(.file):\(.line):\(.column):\(.level):\(.message) [SC\(.code)](https://github.com/koalaman/shellcheck/wiki/SC\(.code))"' \
     | reviewdog -efm="%f:%l:%c:%t%*[^:]:%m" -name="shellcheck" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
-  # github-pr-check (GitHub Check API) doesn't support markdown annotation.
+  # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
   shellcheck -f checkstyle ${INPUT_SHELLCHECK_FLAGS:-'--external-sources'} $(find "${INPUT_PATH:-'.'}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN:-'*.sh'}") \
-    | reviewdog -f="checkstyle" -name="shellcheck" -reporter=github-pr-check -level="${INPUT_LEVEL}"
+    | reviewdog -f="checkstyle" -name="shellcheck" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
 fi
