8bc25b5294b4aa994ae68080c4c9ed8f9d06174b
Felix Bartels <felix@host-consultants.de>
2019-12-14 17:14:13 +0100

    fix "'--external-sources': '--external-sources': openBinaryFile: does not exist (No such file or directory)"
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
   # erroformat: https://git.io/JeGMU
   grep -rIl '^#![[:blank:]]*/bin/\(bash\|sh\|zsh\)' --exclude-dir="${INPUT_EXCLUDE:-.git}" "${INPUT_PATH:-.}" \
-    | xargs shellcheck -f json  "${INPUT_SHELLCHECK_FLAGS:-'--external-sources'}" \
+    | xargs shellcheck -f json  "${INPUT_SHELLCHECK_FLAGS:---external-sources}" \
     | jq -r '.[] | "\(.file):\(.line):\(.column):\(.level):\(.message) [SC\(.code)](https://github.com/koalaman/shellcheck/wiki/SC\(.code))"' \
     | reviewdog -efm="%f:%l:%c:%t%*[^:]:%m" -name="shellcheck" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
   grep -rIl '^#![[:blank:]]*/bin/\(bash\|sh\|zsh\)' --exclude-dir="${INPUT_EXCLUDE:-.git}" "${INPUT_PATH:-.}" \
-    | xargs shellcheck -f checkstyle "${INPUT_SHELLCHECK_FLAGS:-'--external-sources'}" \
+    | xargs shellcheck -f checkstyle "${INPUT_SHELLCHECK_FLAGS:---external-sources}" \
     | reviewdog -f="checkstyle" -name="shellcheck" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 fi
