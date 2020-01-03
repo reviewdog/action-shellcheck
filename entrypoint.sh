176c99e7e03024cf87771b56f9de7461cc306c2a
haya14busa <haya14busa@gmail.com>
2020-01-03 17:57:52 +0000

    Include rule URL to github-check reporter too

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
     | reviewdog -efm="%f:%l:%c:%t%*[^:]:%m" -name="shellcheck" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
-  shellcheck -f checkstyle ${INPUT_SHELLCHECK_FLAGS:-'--external-sources'} $(find "${INPUT_PATH:-'.'}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN:-'*.sh'}") \
-    | reviewdog -f="checkstyle" -name="shellcheck" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+  shellcheck -f json  ${INPUT_SHELLCHECK_FLAGS:-'--external-sources'} $(find "${INPUT_PATH:-'.'}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN:-'*.sh'}") \
+    | jq -r '.[] | "\(.file):\(.line):\(.column):\(.level):\(.message) https://github.com/koalaman/shellcheck/wiki/SC\(.code)"' \
+    | reviewdog -efm="%f:%l:%c:%t%*[^:]:%m" -name="shellcheck" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 fi
